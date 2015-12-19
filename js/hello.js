c5eaa980b7adde1c7af1ee82a0d65ffc83c30e48
Manoj <manojanand432@gmail.com>
2015-12-19 20:23:44 +0530

    Latest changes

--- js/hello.js
+++ js/hello.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
     $('#exampleHome,#exampleOne,#exampleTwo,#exampleThree').DataTable( {
         "pagingType": "full_numbers"
     } );
+    
+    
+    $(".accordion").accordion({
+    	//whether the first section is expanded or not
+    	firstChildExpand: false,
+    	//whether expanding mulitple section is allowed or not
+    	multiExpand: false,
+    	//slide animation speed
+    	slideSpeed: 500,
+    	//drop down icon
+    	dropDownIcon: "&#9660",
+    	});
+    
 } )
